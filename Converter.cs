1ed7723aaae8e5645eb2ea786ebe7f48c4433864
unknown <nkosimova06@gmail.com>
2020-04-06 21:05:15 +0500

    final code with comments

--- Converter.cs
+++ Converter.cs
@@ -1,5 +1,5 @@
 using System;
-namespace Program{
+namespace Program1{
     public class Converter {
         static double usd{ get; set;}
         static double eur {get; set;}
@@ -10,32 +10,32 @@ namespace Program{
             eur = eu;
             rb  = rub;
         }
-        static double ConvertSomToUsd(double som) 
+        public static double ConvertSomToUsd(double som) 
         {
             double us = Math.Round(som/usd,4);
             return us;
         }
-        static double ConvertSomToEur(double som)
+        public static double ConvertSomToEur(double som)
         {
             double eu = Math.Round(som/eur,4);
             return eu;
         }
-        static double ConvertSomToRub(double som)
+        public static double ConvertSomToRub(double som)
         {            
             double rb = Math.Round(som/usd,4);
             return rub;
         }
-        static double ConvertUsdToSom(double us)
+        public static double ConvertUsdToSom(double us)
         {
             double som = Math.Round(us*usd,4);
             return som; 
         }
-        static double ConvertEurToSom(double eu)
+        public static double ConvertEurToSom(double eu)
         {
             double som = Math.Round(eu*eur,4);
             return som;
         }
-        static double ConvertRubToSom(double rb)
+        public static double ConvertRubToSom(double rb)
         {
             double som = Math.Round(rb*rub,4);
             return som;
--- Program.cs
+++ Program.cs
@@ -7,43 +7,117 @@ namespace Program1
         static void Main(string[] args)
         {
            //Problem 2 : Making the Converter class
-           Console.WriteLine("---------------------------------");
-           Console.Write("Here you can convert somoni in usd, eur or rub or vice versa");
-           Console.WriteLine("Please, choose the type of converting:");
-           Console.WriteLine("Convert somoni to dollar - 1");
-           Console.WriteLine("Convert somoni to euro   - 2");
-           Console.WriteLine("Convert somoni to ruble  - 3");
-           Console.WriteLine("Convert dollar to somoni - 4");
-           Console.WriteLine("Convert euro to somoni   - 5");
-           Console.WriteLine("Convert ruble to somoni  - 6");
-           int choice = int.Parse(Console.ReadLine());
-           Console.WriteLine("Now, write down the sum: ");
-           int summ = int.Parse(Console.ReadLine());
+            Converter n = new Converter(0.098, 0.091, 7.48);
+            int choice;
+            Console.WriteLine("---------------------------------");
+            Console.Write("Here you can convert somoni in usd, eur or rub or vice versa");
+            Console.WriteLine("Please, choose the type of converting:");
+            Console.WriteLine("Convert somoni to dollar - 1");
+            Console.WriteLine("Convert somoni to euro   - 2");
+            Console.WriteLine("Convert somoni to ruble  - 3");
+            Console.WriteLine("Convert dollar to somoni - 4");
+            Console.WriteLine("Convert euro to somoni   - 5");
+            Console.WriteLine("Convert ruble to somoni  - 6");
+                while (true) 
+            {
+                choice = int.Parse(Console.ReadLine());
+                if (choice < 1 || choice > 6) {
+                    Console.WriteLine("Wrong command. Please, try again");
+                } else {
+                    break;
+                }
+            }
+            Console.WriteLine("Now, write down the sum: ");
+           double summ = double.Parse(Console.ReadLine());
            switch(choice)
            {
                case 1:
-                   Console.WriteLine($"{summ} somoni == {ConvertSomToUsd(summ)} dollar");
+                   Console.WriteLine($"{summ} somoni == {Converter.ConvertSomToUsd(summ)} dollar");
                    break;
                case 2:
-                   Console.WriteLine($"{summ} somoni == {ConvertSomToEur(summ)} euro");
+                   Console.WriteLine($"{summ} somoni == {Converter.ConvertSomToEur(summ)} euro");
                    break;
                case 3:
-                   Console.WriteLine($"{summ} somoni == {ConvertSomToRub(summ)} ruble");
+                   Console.WriteLine($"{summ} somoni == {Converter.ConvertSomToRub(summ)} ruble");
                    break;
                case 4:
-                   Console.WriteLine($"{summ} dollars == {ConvertUsdToSom(summ)} somoni");
+                   Console.WriteLine($"{summ} dollars == {Converter.ConvertUsdToSom(summ)} somoni");
                    break;
                case 5:
-                   Console.WriteLine($"{summ} euro == {ConvertEurToSom(summ)} somoni");
+                   Console.WriteLine($"{summ} euro == {Converter.ConvertEurToSom(summ)} somoni");
                    break;
                case 6:
-                   Console.WriteLine($"{summ} ruble == {ConvertRubToSom(summ)} somoni");
+                   Console.WriteLine($"{summ} ruble == {Converter.ConvertRubToSom(summ)} somoni");
                    break;
                default:
-                   Console.WriteLine("Wrong command. Please, try again");
                    break;
            }
 
         }
+
+            class Converter 
+        {
+            public static double usd{ get; set;}
+            public static double eur {get; set;}
+            public static double rub {get; set;}
+            public Converter(double us, double eu, double rb)
+            {
+                usd = us;
+                eur = eu;
+                rub  = rb;
+            }
+            public static double ConvertSomToUsd(double som) 
+            {
+                double us = Math.Round(som*usd,4);
+                return us;
+            }
+            public static double ConvertSomToEur(double som)
+            {
+                double eu = Math.Round(som*eur,4);
+                return eu;
+            }
+            public static double ConvertSomToRub(double som)
+            {            
+                double rb = Math.Round(som*rub,4);
+                return rb;
+            }
+            public static double ConvertUsdToSom(double us)
+            {
+                if (usd != 0)
+                {
+                    double som = Math.Round(us/usd,4);
+                    return som; 
+                } else {
+                    Console.WriteLine("Error1");
+                    return 0;
+                }
+            }
+            public static double ConvertEurToSom(double eu)
+            {
+                if (eur != 0)
+                {
+                    double som = Math.Round(eu/eur,4);
+                    return som;
+                } else {
+                    Console.WriteLine("Error2");
+                    return 0;
+                }
+            }
+            public static double ConvertRubToSom(double rb)
+            {
+                if (rub != 0)
+                {
+                    double som = Math.Round(rb/rub,4);
+                    return som;
+                } else {
+                    Console.WriteLine("Error3");
+                    return 0;
+                }
+            }
+        }
+    /* При создании класса Converter я столкнулась с трудностью в записи статичных методов
+    в главной программе Main, также много времени заняло отлаживание кода.
+    После неудачных попыток написать класс в отдельном файле отдельно от Program.cs, я оставила класс в общем коде.*/
+      
     }
 }
